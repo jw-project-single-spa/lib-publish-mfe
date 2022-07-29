92957c2f4740d8cd030c32086c6843738c4352c1
Eduardo Dallmann <eduardo.dallmann@ambevtech.com.br>
2022-07-29 13:46:02 -0300

    final

--- bin/index.ts
+++ bin/index.ts
@@ -2,26 +2,49 @@
 import yargs from "yargs/yargs";
 import { createClient } from "@supabase/supabase-js";
 import fs from "fs";
-import FormData from "form-data";
-import fetch from "node-fetch";
+import axios from "axios";
 
 async function run() {
-  const { file } = await yargs(process.argv).options({
-    file: { type: "array" },
+  const { fileName, fileAddress, mfeName, supabaseAuth } = await yargs(
+    process.argv
+  ).options({
+    fileName: { type: "string" },
+    fileAddress: { type: "string" },
+    mfeName: { type: "string" },
+    supabaseAuth: { type: "string" },
   }).argv;
 
-  console.log(file);
+  if (!fileName || !fileAddress || !mfeName || !supabaseAuth) {
+    console.error("faltam pametros");
+    process.exit(1);
+  }
 
-  const supabase = createClient(
-    "https://ufuubufsowyauanuzbii.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmdXVidWZzb3d5YXVhbnV6YmlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTg4MDAwNjcsImV4cCI6MTk3NDM3NjA2N30.Ox5NWDZYy0ssP11hl634vZ4Sdt28caZSz42LjsbydJk"
-  );
+  const url = "https://ufuubufsowyauanuzbii.supabase.co";
+
+  const supabase = createClient(url, supabaseAuth);
+
+  await supabase.storage.from("bundle").remove([fileName]);
 
   const { data, error } = await supabase.storage
-    .from("public")
-    .upload("index.js", await fs.promises.readFile("dist/index.js"));
-
+    .from("bundle")
+    .upload(fileName, await fs.promises.readFile(fileAddress));
   console.log(data, error);
+
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+  console.log("upload to supabase: ", data);
+
+  try {
+    await axios.post("https://jw-service-list-mfe.herokuapp.com/", {
+      name: mfeName,
+      url: `${url}/storage/v1/object/public/${data?.Key}`,
+    });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
 }
 
 run();
