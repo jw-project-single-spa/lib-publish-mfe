6d275e8233b243bd2629e7335c43cea8c42d874f
Eduardo Dallmann <eduardodallmann@gmail.com>
2022-08-22 18:05:33 -0300

    change to firebase

--- bin/index.ts
+++ bin/index.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import yargs from "yargs/yargs";
-import { createClient } from "@supabase/supabase-js";
-import fs from "fs";
-import axios from "axios";
+import { initializeApp, cert } from "firebase-admin/app";
+import { getStorage } from "firebase-admin/storage";
+import { firestore } from "firebase-admin";
 
 function error(e: unknown) {
   console.error(e);
@@ -17,7 +17,7 @@ async function run() {
     fileName,
     fileAddress,
     mfeName,
-    supabaseAuth,
+    firebaseAuth,
   } = await yargs(process.argv).options({
     activeWhen: { type: "string" },
     exact: { type: "boolean" },
@@ -25,41 +25,63 @@ async function run() {
     fileName: { type: "string" },
     fileAddress: { type: "string" },
     mfeName: { type: "string" },
-    supabaseAuth: { type: "string" },
+    firebaseAuth: { type: "string" },
   }).argv;
 
-  if (!fileName || !fileAddress || !mfeName || !supabaseAuth) {
+  if (!fileName || !fileAddress || !mfeName || !firebaseAuth) {
     return error("faltam pametros");
   }
 
-  const url = "https://ufuubufsowyauanuzbii.supabase.co";
+  // -----
+  // auth
+  var serviceAccount = require("../auth.json");
 
-  const supabase = createClient(url, supabaseAuth);
+  initializeApp({
+    credential: cert(serviceAccount),
+    databaseURL: "https://jw-project-58cb8-default-rtdb.firebaseio.com",
+    storageBucket: "gs://jw-project-58cb8.appspot.com",
+  });
 
-  await supabase.storage.from("bundle").remove([fileName]);
+  // -----
+  // upload mfe bundle to store
+  const f = await getStorage()
+    .bucket()
+    .upload(fileAddress, {
+      destination: `bundle/${fileName}`,
+      public: true,
+    });
+  const [url] = await f[0].getSignedUrl({
+    action: "read",
+    expires: "2100-01-01",
+  });
+  console.info("upload do bundle em:", url);
 
-  const { data, error: errorStorage } = await supabase.storage
-    .from("bundle")
-    .upload(fileName, await fs.promises.readFile(fileAddress));
-  console.log(data, errorStorage);
-  if (errorStorage) {
-    return error(errorStorage);
-  }
-  console.log("upload to supabase: ", data);
+  // -----
+  // register mfe on db
+  try {
+    const db = firestore().collection("mfes");
 
-  const { error: errorUpsert } = await supabase.from("mfe").upsert(
-    {
+    // procura se mfe já existe
+    const mfeExist = await db.where("name", "==", mfeName).get();
+    const body = {
       name: mfeName,
-      url: `${url}/storage/v1/object/public/${data?.Key}`,
+      url,
       activeWhen,
       exact,
       isParcel,
-    },
-    { onConflict: "name" }
-  );
+    };
+    let save: firestore.WriteResult;
 
-  if (errorUpsert) {
-    return error(errorUpsert);
+    if (!mfeExist.empty) {
+      // se existe - atualiza
+      save = await mfeExist.docs[0].ref.update(body);
+    } else {
+      // se não - cria novo
+      save = await db.doc().set(body);
+    }
+    console.info("MFE registrado");
+  } catch (errorDB) {
+    error(errorDB);
   }
 }
 
