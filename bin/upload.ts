dbd4c273334bdccbbaed715ee7255ef2417a0d4d
Eduardo Dallmann <eduardodallmann@gmail.com>
2022-09-20 19:20:55 -0300

    ignore folders

--- bin/upload.ts
+++ bin/upload.ts
@@ -1,9 +1,13 @@
 import fs from "fs";
+import path from "path";
 import { getStorage } from "firebase-admin/storage";
 
 export async function upload(folderAddress: string, destinationFolder: string) {
   const listFiles = fs
     .readdirSync(folderAddress)
+    .filter(
+      (file) => !fs.statSync(path.join(folderAddress, file)).isDirectory()
+    )
     .filter((f) => !f.includes(".ts"));
 
   for (const fileIter of listFiles) {
